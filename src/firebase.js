b26755a65f556e9323dbf700ae5d4897e2273a61
Super Promiser <superpromiser@gmail.com>
2021-01-09 18:41:55 +0800

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCWg-5PxbPrL9qw-O_AiYmTgUcC9WrOeIE",
+    authDomain: "slack-clone-ad250.firebaseapp.com",
+    projectId: "slack-clone-ad250",
+    storageBucket: "slack-clone-ad250.appspot.com",
+    messagingSenderId: "166159592699",
+    appId: "1:166159592699:web:1c71bf5b8cb0c2fc89d45e",
+    measurementId: "G-XM6TSRH7DZ"
+};
